5819ff4bedfa7a72208021b12e5e5b89620db17c
Tawsif Adnan <tawsif.adnan22@gmail.com>
2023-05-19 07:40:13 +0600

    dynamic url created for single toy view details button

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,7 +9,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s92qhby.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,6 +31,13 @@ async function run() {
 
     app.get("/toyGalaxyCategory", async (req, res) => {
       const result = await shopByCategoryCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/toyGalaxyCategory/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await shopByCategoryCollection.findOne(query);
       res.send(result);
     });
 
